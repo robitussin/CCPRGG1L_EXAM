548424d628f780662d38a3a581b121be4988ca89
robitussin <elizerponiojr@gmail.com>
2022-11-06 21:35:45 +0800

    Create FinalExam.java

---
+++ final_exam/FinalExam.java
@@ -0,0 +1,72 @@
+public class FinalExam {
+    public static void main(String[] args) throws Exception {
+
+        // Print the words in their given pattern.
+
+        char[][] array = {
+                { 'R', 'E', 'I', 'M', 'A', 'E', 'R', 'S', 'G', 'T' },
+                { 'E', 'V', 'D', 'B', 'D', 'V', 'I', 'A', 'B', 'I' },
+                { 'E', 'L', 'R', 'O', 'F', 'S', 'A', 'W', 'U', 'K' },
+                { 'B', 'G', 'C', 'E', 'I', 'L', 'C', 'J', 'H', 'T' },
+                { 'S', 'S', 'K', 'G', 'D', 'Z', 'H', 'S', 'T', 'O' },
+                { 'V', 'M', 'J', 'A', 'V', 'H', 'Z', 'S', 'I', 'K' },
+                { 'C', 'H', 'I', 'L', 'L', 'T', 'O', 'P', 'G', 'M' },
+                { 'Y', 'S', 'P', 'I', 'T', 'A', 'P', 'R', 'W', 'D' },
+                { 'G', 'B', 'D', 'S', 'T', 'K', 'P', 'W', 'S', 'O' },
+                { 'G', 'T', 'L', 'H', 'Q', 'X', 'Q', 'L', 'O', 'E' },
+        };
+
+        // =============================================================
+        // ALAK - (10 points)
+        System.out.println(" ");
+        System.out.println("1. ALAK ");
+
+        // =============================================================
+        // GITHUB - (10 points)
+        System.out.println(" ");
+        System.out.println("2. GITHUB ");
+
+        // =============================================================
+        // SISIG - (10 points)
+        System.out.println(" ");
+        System.out.println("3. SISIG ");
+
+        // =============================================================
+        // VSCODE - (10 points)
+        System.out.println(" ");
+        System.out.println("4. VSCODE ");
+
+        // =============================================================
+        // BEER - (10 points)
+        System.out.println(" ");
+        System.out.println("5. BEER ");
+
+        // =============================================================
+        // JAVA - (10 points)
+        System.out.println(" ");
+        System.out.println("6. JAVA ");
+
+        // =============================================================
+        // TIKTOK - (10 points)
+        System.out.println(" ");
+        System.out.println("7. TIKTOK ");
+
+        // =============================================================
+        // CHILLTOP - (10 points)
+        System.out.println(" ");
+        System.out.println("8. CHILLTOP ");
+
+        // =============================================================
+        // REDHORSE - (10 points)
+        System.out.println(" ");
+        System.out.println("9. REDHORSE ");
+
+        // =============================================================
+        // TIPSY - (10 points)
+        System.out.println(" ");
+        System.out.println("10. TIPSY ");
+
+        // =============================================================
+
+    }
+}
